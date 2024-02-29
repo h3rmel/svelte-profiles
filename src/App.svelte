4a0610de874253248ae365aebd3ae5a5b7725321
H3rmel <isaachermel@gmail.com>
2024-02-29 01:26:41 -0300

    docs: added TODOS

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   import CardUser from "@/components/CardUser.svelte";
   import ReposGrid from "@/components/Repos/ReposGrid.svelte";
   import ReposList from "@/components/Repos/ReposList.svelte";
-  
+
   // Switch to Local services if the request for GitHub API exceeded.
   import { searchUserLocal, searchUserReposLocal } from "@/services/index";
   // import { searchUser, searchUserRepos } from "@/services/index";
@@ -96,6 +96,7 @@
     <p class="text-neutral">Search someone on Github!</p>
   </hgroup>
   <!-- Form (Input + Submit Button) -->
+  <!-- TODO: Transformar formulário em componente -->
   <form
     on:submit|preventDefault={handleSubmit}
     class="card flex-row p-4 border-[1px] border-border flex gap-2"
@@ -111,6 +112,7 @@
     </button>
   </form>
   <!-- Alert -->
+  <!-- TODO: Transformar alerta em componente -->
   {#if fetchError.status !== 0}
     <div class="alert alert-error max-w-[24rem]">
       <ErrorIcon />
@@ -125,6 +127,7 @@
     <CardUser {user} />
   {/if}
   <!-- Repos -->
+  <!-- TODO: Transformar seção dos repositórios em componente -->
   {#if repos}
     <section class="flex items-center gap-2">
       <p class="text-base">Visão:</p>
